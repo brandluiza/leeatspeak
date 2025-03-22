5810a5545394e0e50ca608fa21605cf34c8d333e
Karla <e107ti@etec.sp.gov.br>
2025-03-21 21:53:54 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿String textoDigitado;
+String respostaDigitada;
+
+Console.WriteLine("Texto a ser traduzido...");
+textoDigitado = Console.ReadLine();
+textoDigitado = textoDigitado.Replace("A", "4")
+                             .Replace("E","3")
+                             .Replace("I", "1")
+                             .Replace("O", "0")
+                             .Replace("T", "7")
+                             .Replace("S", "5")
+                             .Replace("a", "4")
+                             .Replace("e","3")
+                             .Replace("i", "1")
+                             .Replace("o", "0")
+                             .Replace("t", "7")
+                             .Replace("s", "5");
+ Console.WriteLine($"{textoDigitado}!");
+
